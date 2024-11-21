8f346eb95525aa83f0385e898e988b17e47fb9d7
giahung123456 <dogiahungtplc@gmail.com>
2024-11-22 03:49:54 +0700

    UPLOAD SS3_05

---
+++ ss3_05.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+  main(){
+  	printf("danh sach sinh vien\n");
+	printf("stt:1\nho va ten:nguyen van A\ntuoi:20\nso dien thoai:0904488481\nemail:anv@rikkeiacademy.com");
+  	printf("stt:2\nho va ten:nguyen van B\ntuoi:21\nso dien thoai:0904488482\nemail:bnv@rikkeiacademy.com");
+  	printf("stt:3\nho va ten:nguyen van C\ntuoi:18\nso dien thoai:0904488483\nemail:cnv@rikkeiacademy.com");
+  	printf("stt:4\nho va ten:nguyen van D\ntuoi:19\nso dien thoai:0904488484\nemail:dnv@rikkeiacademy.com");
+  	printf("stt:5\nho va ten:nguyen van E\ntuoi:20\nso dien thoai:0904488485\nemail:env@rikkeiacademy.com");
+  	printf("stt:6\nho va ten:nguyen van F\ntuoi:21\nso dien thoai:0904488486\nemail:fnv@rikkeiacademy.com");
+  	printf("stt:7\nho va ten:nguyen van G\ntuoi:23\nso dien thoai:0904488487\nemail:gnv@rikkeiacademy.com");
+  	printf("stt:8\nho va ten:nguyen van H\ntuoi:19\nso dien thoai:0904488488\nemail:hnv@rikkeiacademy.com");
+  	printf("stt:9\nho va ten:nguyen van I\ntuoi:18\nso dien thoai:0904488489\nemail:inv@rikkeiacademy.com");
+  	printf("stt:10\nho va ten:nguyen van K\ntuoi:21\nso dien thoai:0904488480\nemail:knv@rikkeiacademy.com");
+  }
+  
+  
